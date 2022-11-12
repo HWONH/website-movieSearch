5c992d73acda4fcfea85d0e47a5d09055a551fd6
HWONH <95hyun408@naver.com>
2022-11-12 18:24:50 +0900

    revise about page

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'HYEWON',
+    email: 'gpdhd408@naver.com',
+    blog: 'https://hwonh.tistory.com/',
+    phone: '+82-10-3070-0861',
+    image: '~/assets/logo.png'
+  })
+}
